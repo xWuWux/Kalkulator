4b9500a20701c489b572af799dc80fcafd86329b
xWuWux <wojteklobo@gmail.com>
2019-09-28 19:03:34 +0200

    Initial commit

--- Kalkulator WSB/Program.cs
+++ Kalkulator WSB/Program.cs
@@ -9,6 +9,18 @@ namespace Kalkulator_WSB
     class Program
     {
         static void Main(string[] args)
+           
+            while(true)
+           {
+            Console.Writeline("0.Exit");
+            Console.Writeline("1.ADD");
+            Console.Writeline("2.DIFF");
+            int menuOption = Convert.ToInt32(Console.ReadLine());
+            if (menuOption == 0)
+            {
+            break;
+            }
+           }
         {
         }
     }
