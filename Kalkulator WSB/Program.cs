0cacfd42808d6bde3f13950b7c24eaeb17e66005
studentwsb <studentwsb@STUDENTWSB>
2019-09-28 18:38:16 +0200

    Add project files.

---
+++ Kalkulator WSB/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kalkulator_WSB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
